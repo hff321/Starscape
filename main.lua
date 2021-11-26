62a46a2bbbb2471e3d21907bd130a492796e74ec
Harrison Fisher <HarrisonFisher2018@gmail.com>
2021-11-26 14:26:21 -0800

    fixed json?

---
+++ main.lua
@@ -0,0 +1,19 @@
+
+
+local dependencies = {
+    ["JSON"] = "https://raw.githubusercontent.com/NoTwistedHere/Storage/main/Other/JSONWithUserdata",
+    ["YourIP"] = "https://myip.wtf/text",
+    ["IP_List"] = "https://raw.githubusercontent.com/NoTwistedHere/Storage/main/Other/JSONWithUserdata"
+}
+
+local JSON = loadstring(game:HttpGet(dependencies["JSON"], true))()
+local YourIP = string.gsub(game:HttpGet(dependencies["YourIP"]), "\n", "")
+local IP_List = JSON:Decode(game:HttpGet(dependencies["IP_List"]))
+
+for i,v in pairs(IP_List) do
+
+    rconsoleprint(tostring(i))
+    rconsoleprint("     ")
+    rconsoleprint(tostring(v))
+    rconsoleprint("\n")
+end
